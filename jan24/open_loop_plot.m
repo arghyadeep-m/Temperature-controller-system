8f4ab4e9830333214bfb3d165fef8c5636371349
arghyadeep-m <121192597+arghyadeep-m@users.noreply.github.com>
2024-06-04 16:29:04 +0530

    p, i, d gain calculating block removed
    According to Zeigler-Nichol tuning rule K_p, K_i, K_d values are not same for different controllers (i.e. P, PI, PID). Hence, the calculation of those gains are done individually at specific controller's code.

--- jan24/open_loop_plot.m
+++ jan24/open_loop_plot.m
@@ -36,22 +36,7 @@ K = ( f(n) - f(1) )/input_volts;
 T2 = x(1);
 T1 = x(2) - x(1);
 
-% p setting
-K_p = ( 1.2*T1 )/( K*T2 );
-K_pmax = 0.2;
-p = (K_p/K_pmax);
-
-% i setting
-K_i = 1/( 2*T2 );
-K_imax = 1/28;
-i = K_i/K_imax;
-
-% d setting
-K_d = 0.5*T2;
-K_dmax = 23.5;
-d = K_d/K_dmax;
-
 % Writing to a file
 fileID = fopen('open_loop_const.txt', 'w');
-fprintf(fileID, '%f\n%f\n%f\n%f\n%f\n%f\n', K, x(2), x(1), K_p, K_i, K_d);
+fprintf(fileID, '%f\n%f\n%f\n', K, T1, T2);
 fclose(fileID);

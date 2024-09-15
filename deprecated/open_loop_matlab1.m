999348f7a968d8e8ee33102023a8f03484ebdf86
arghyadeep-m <121192597+arghyadeep-m@users.noreply.github.com>
2024-09-16 00:56:21 +0530

    no unnecessary folders, after doing lot of things

--- deprecated/open_loop_matlab1.m
+++
@@ -1,9 +0,0 @@
-%: Read the excel file using readtable function
-rawTable = readtable('open loop.xlsx','Sheet','Sheet1');
-x = rawTable.TimeInSeconds; %: get the excel column, Header1 (header name)
-y = rawTable.TemperatureInDegC; %: get the excel column, Header2 (header name)
-figure;
-plot(x,y)
-xlabel('Time in Seconds');
-ylabel('Temperature in deg C');
-grid;
